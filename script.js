78da892ea28dcbf0a7c47126cd6b7ff50ebd91e1
Maggie Berver <maggieberver@gmail.com>
2026-03-02 09:51:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+class Lista {
+    constructor() {
+        tareas = [];
+    }
+    agregar() {
+        
+    }
+    modificar() {
+
+    }
+    tachar() {
+
+    }
+    eliminar() {
+
+    }
+}
+
+const addTaskButton = document.getElementById("add-task")
+addTaskButton.addEventListener("click", function() {
+    alert("Botón de agregar tarea clickeado");
+});
